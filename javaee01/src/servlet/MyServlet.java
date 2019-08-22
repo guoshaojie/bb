5fbca43f58d8ffe31c87730c5acd4c915b2938ed
guo <guo_1@163.com>
2019-08-22 10:54:10 +0800

    egit update01

---
+++ javaee01/src/servlet/MyServlet.java
@@ -0,0 +1,35 @@
+package servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MyServlet extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		resp.setContentType("text/html;charset=utf-8");
+		
+		
+		req.getRequestDispatcher("index.html").forward(req, resp);
+		
+		
+		
+		
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doGet(req,resp);
+	}
+
+
+
+	
+
+}
